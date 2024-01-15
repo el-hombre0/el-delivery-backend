823b260e5b7946e0e20c5a186c008a6e2cef3213
Stanislav Efimtsev <evendot@yandex.ru>
2024-01-15 22:35:19 +0300

    The security configuration is done

--- src/main/java/com/eldelivery/server/Security/WebSecurityConfig.java
+++ src/main/java/com/eldelivery/server/Security/WebSecurityConfig.java
@@ -2,7 +2,10 @@ package com.eldelivery.server.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,6 +13,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -55,5 +60,40 @@ public class WebSecurityConfig {
 		return username -> repository.findByEmail(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
+	}
+
+	/**
+	 * DAO для получения UserDetails, кодирования пароля и т.д.
+	 * 
+	 * @return Провайдер аутентификации
+	 */
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+
+	/**
+	 * Непосредственная аутентификация
+	 * 
+	 * @param config Конфигурация аутентификации
+	 * @return Аутентификация
+	 * @throws Exception
+	 */
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	/**
+	 * Метод шифрования пароля
+	 * 
+	 * @return Шифровальщик пароля
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
